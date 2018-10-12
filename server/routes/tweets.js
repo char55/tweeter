7053f77079e38371743bf112502a45e0f0ef13f2
char55 <haras.charlotte@gmail.com>
2018-10-12 17:47:27 +0000

    formatting

--- server/routes/tweets.js
+++ server/routes/tweets.js
@@ -31,7 +31,6 @@ module.exports = function(DataHelpers) {
         text: req.body.text
       },
       created_at: Date.now(),
-      likes: 0
     };
 
     DataHelpers.saveTweet(tweet, (err) => {
@@ -43,13 +42,6 @@ module.exports = function(DataHelpers) {
     });
   });
 
-
-  // tweetsRoutes.post("/likes", function(req, res) {
-  //   DataHelpers.updateTweet(date, likesUpdated, (err, tweets) => {
-
-  //   })
-
-  // })
 
   return tweetsRoutes;
 
